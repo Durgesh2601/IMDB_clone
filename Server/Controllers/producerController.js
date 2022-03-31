f196f6ddf94aa6afd167a23ba14fcaaf89d4529d
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-03-31 23:16:59 +0530

    updated

--- Server/Controllers/producerController.js
+++ Server/Controllers/producerController.js
@@ -1,54 +1,66 @@
-const {Router}=require('express')
+const { Router } = require("express");
 
-const router=Router()
+const router = Router();
 
-const Producer=require('../Models/producerModel')
+const Producer = require("../Models/producerModel");
 
-const {newToken} =require('./actorController')
+const { newToken } = require("./actorController");
 
-const authenticate=require('../Middleware/authentication')
+const authenticate = require("../Middleware/authentication");
 
-router.post("",async(req,res)=>{
-    try{
-        let producer=await Producer.findOne({Name:req.body.Name}).lean().exec()
-        if(producer){
-            return res.status(400).send({message:"Producer With that Name already Exists"})
-        }
-        producer=await Producer.create(req.body)
-      let token=newToken(producer)
-      return res.status(201).send({producer,token})
-    }catch(err){
-        return res.status(500).send(err.message)
+router.post("", async (req, res) => {
+  try {
+    let producer = await Producer.findOne({ Name: req.body.Name })
+      .lean()
+      .exec();
+    if (producer) {
+      return res
+        .status(400)
+        .send({ message: "Producer with that name already exists" });
     }
-})
-router.get('',async(req,res)=>{
-    try{
-        const producer=await Producer.find().populate('Movies').lean().exec()
-  
-        return res.status(200).send(producer)
-    }catch(err){
-        return res.status(500).send(err.message)
-    }
-  })
+    producer = await Producer.create(req.body);
+    let token = newToken(producer);
+    return res.status(201).send({ producer, token });
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+router.get("", async (req, res) => {
+  try {
+    const producer = await Producer.find().populate("Movies").lean().exec();
 
-  router.patch('/:id',authenticate,async(req,res)=>{
-      try{
-        let producer=await Producer.findByIdAndUpdate(req.params.id,{
-            $push:{Movies:req.body.Movies}
-        },{new:true}).populate('Movies').lean().exec()
-        return res.send(producer)
-      }catch(err){
-        return res.status(500).send(err.message) 
-      }
-  })
-  router.delete('/:id',authenticate,async(req,res)=>{
-    try{
-      let producer=await Producer.findByIdAndDelete(req.params.id).lean().exec()
-      return res.send(producer)
-    }catch(err){
-      return res.status(500).send(err.message) 
-    }
-})
+    return res.status(200).send(producer);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
+router.patch("/:id", authenticate, async (req, res) => {
+  try {
+    let producer = await Producer.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { Movies: req.body.Movies },
+      },
+      { new: true }
+    )
+      .populate("Movies")
+      .lean()
+      .exec();
+    return res.send(producer);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+router.delete("/:id", authenticate, async (req, res) => {
+  try {
+    let producer = await Producer.findByIdAndDelete(req.params.id)
+      .lean()
+      .exec();
+    return res.send(producer);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
-module.exports=router
+module.exports = router;
--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,7 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-module.exports=()=>{
-    return mongoose.connect('mongodb+srv://amanfw13064:anonymous@cluster0.66g2t.mongodb.net/test')
-}
+module.exports = () => {
+  return mongoose.connect(
+    "mongodb+srv://amanfw13064:anonymous@cluster0.66g2t.mongodb.net/test"
+  );
+};
