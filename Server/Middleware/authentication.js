20a1ff02c81fb1b8a8ee86200c340bb44d0b7171
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-03-31 23:22:39 +0530

    updated

--- Server/Middleware/authentication.js
+++ Server/Middleware/authentication.js
@@ -1,30 +1,29 @@
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const verifyToken=(token)=>{
-    return new Promise((resolve,reject)=>{
-        jwt.verify(token,process.env.JWT_SECRET_KEY,(err,decoded)=>{
-            if(err) reject(err)
-            resolve(decoded)
-        })
-    })
-}
+const verifyToken = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+      if (err) reject(err);
+      resolve(decoded);
+    });
+  });
+};
 
-module.exports= async(req,res,next)=>{
-        if(!req.headers.authorization){
-            return res.status(400).send("Bad Request")
-        }
-        let BearerToken=req.headers.authorization
-        if(!BearerToken.startsWith('Bearer')){
-            return res.status(400).send("Bad Request")
-        }
-        let token=BearerToken.split(" ")[1]
-        // let tok
-        try{
-           await verifyToken(token)    
-        }catch(err){
-            return res.status(400).send("Token is not valid")
-        }
-        next()
-    }
+module.exports = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(400).send("Bad request");
+  }
+  let BearerToken = req.headers.authorization;
+  if (!BearerToken.startsWith("Bearer")) {
+    return res.status(400).send("Bad request");
+  }
+  let token = BearerToken.split(" ")[1];
+  try {
+    await verifyToken(token);
+  } catch (err) {
+    return res.status(400).send("Token is not valid");
+  }
+  next();
+};
