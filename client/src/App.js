5c520c3a70970a1a504caa7fb60b318ac82842f6
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-04-05 22:23:48 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import { Homepage } from './components/Home/Homepgae';
 import { MoviesList } from './components/Movies/MoviesList';
 import { SignIn } from './components/SignIn/SignIn';
 import { SignUp } from './components/SignUp/SignUp';
+import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
-      <MoviesList/>
-      <SignIn/>
-      <SignUp/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/movies' element={<MoviesList/>}></Route>
+        <Route path='/signin' element={<SignIn/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+      </Routes>
     </div>
   );
 }
