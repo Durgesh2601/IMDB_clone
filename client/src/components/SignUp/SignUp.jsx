722573e27d95764398c0c358cb81ab5a5301b1ac
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-04-05 22:46:42 +0530

    modified signup page

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -3,8 +3,13 @@ import "./SignUp.css";
 export const SignUp = () => {
     return <>
     <Navbar/>
-    <div className="contSignUp">
-
+    <div className="contSignUp mt-4">
+        <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+        <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+        </div>
     </div>
     </>
 }
